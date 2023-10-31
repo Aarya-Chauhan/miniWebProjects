ea4b938b2d6049b27be5ee442b350b0afd470e4d
Aarya Chauhan <aaryachauhan11@gmail.com>
2023-10-31 10:06:40 +0530

    styling spotify

---
+++ spotifyUI/index.js
@@ -0,0 +1,8 @@
+document.getElementById("clkbtn").addEventListener("click", function(){
+    var s1 = new Audio("songs/1.mp3");
+    s1.play();
+    console.log("btn clicked")
+    console.log("sound playing")
+}
+);
+
